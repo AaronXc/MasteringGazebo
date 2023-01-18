5e4936a7c3e2df0eb2f69401a8a97db05d277a56
AaronXc <aaronwilcox2000@gmail.com>
2023-01-18 02:03:11 -0330

    remove build and devel directories

--- catkin_ws/build/robot_description_g/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/catkin_ws/src/robot_description_g
-# Build directory: /home/user/catkin_ws/build/robot_description_g
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/writing_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/catkin_ws/src/writing_plugins
-# Build directory: /home/user/catkin_ws/build/writing_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
