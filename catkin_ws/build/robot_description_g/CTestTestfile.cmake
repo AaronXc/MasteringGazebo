b4bcb6038248ab7d6cc0d695abdf1808c91747a4
AaronXc <aaronwilcox2000@gmail.com>
2023-01-18 00:49:56 -0330

    first commit

---
+++ catkin_ws/build/robot_description_g/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src/robot_description_g
+# Build directory: /home/user/catkin_ws/build/robot_description_g
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
